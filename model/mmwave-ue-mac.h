421459ff1bf5da81d2769551e99a45b228f2c8f5
Natale Patriciello <natale.patriciello@cttc.cat>
2020-02-17 09:59:52 +0100

    ue-mac: Fixed GPL headers

--- model/mmwave-ue-mac.h
+++ model/mmwave-ue-mac.h
@@ -1,23 +1,21 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
-*   Copyright (c) 2011 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
-*   Copyright (c) 2015 NYU WIRELESS, Tandon School of Engineering, New York University
-*   Copyright (c) 2019 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
-*
-*   This program is free software; you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License version 2 as
-*   published by the Free Software Foundation;
-*
-*   This program is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program; if not, write to the Free Software
-*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*
-*/
+ *   Copyright (c) 2019 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2 as
+ *   published by the Free Software Foundation;
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 
 #ifndef MMWAVE_UE_MAC_H
 #define MMWAVE_UE_MAC_H
@@ -25,11 +23,15 @@
 
 #include "mmwave-mac.h"
 #include "mmwave-phy-mac-common.h"
-#include "mmwave-phy-sap.h"
+
 #include <ns3/lte-ue-cmac-sap.h>
 #include <ns3/lte-ccm-mac-sap.h>
 
 namespace ns3 {
+
+class MmWaveUePhySapUser;
+class MmWavePhySapProvider;
+class MmWaveControlMessage;
 
 /**
  * \ingroup ue
@@ -240,7 +242,7 @@ private:
   Ptr<UniformRandomVariable> m_raPreambleUniformVariable;
   uint8_t m_raPreambleId {0};
   uint8_t m_raRnti {0};
-  uint16_t m_imsi {0}; ///< IMSI
+  uint64_t m_imsi {0}; ///< IMSI
 
   struct UlHarqProcessInfo
   {
