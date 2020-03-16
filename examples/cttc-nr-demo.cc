146510dde5acf1c26455aa63e175d1248f2ccc4e
Nat <natale.patriciello@gmail.com>
2020-03-16 14:34:24 +0100

    examples: cttc-nr-demo updated

--- examples/cttc-nr-demo.cc
+++ examples/cttc-nr-demo.cc
@@ -1,7 +1,6 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
- -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*-
-
- *   Copyright (c) 2018 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+ *   Copyright (c) 2019 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License version 2 as
@@ -18,6 +17,29 @@
  *
  */
 
+/**
+ * \ingroup examples
+ * \file cttc-nr-demo.cc
+ * \brief A cozy, simple, NR demo (in a tutorial style)
+ *
+ * This example describes how to setup a simulation using the 3GPP channel model
+ * from TR 38.900. This example consists of a simple grid topology, in which you
+ * can choose the number of gNbs and UEs. Have a look at the possible parameters
+ * to know what you can configure through the command line.
+ *
+ * The example will print on-screen the end-to-end result of one (or two) flows,
+ * as well as writing them on a file.
+ *
+ * \code{.unparsed}
+$ ./waf --run "cttc-nr-demo --Help"
+    \endcode
+ *
+ */
+
+/*
+ * Include part. Often, you will have to include the headers for an entire module;
+ * do that by including the name of the module you need with the suffix "-module.h".
+ */
 
 #include "ns3/core-module.h"
 #include "ns3/config-store.h"
@@ -26,30 +48,57 @@
 #include "ns3/internet-apps-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/mobility-module.h"
-#include "ns3/log.h"
-#include "ns3/point-to-point-helper.h"
+#include "ns3/point-to-point-module.h"
 #include "ns3/flow-monitor-module.h"
-#include "ns3/mmwave-helper.h"
-#include "ns3/nr-point-to-point-epc-helper.h"
-#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/nr-module.h"
 #include "ns3/config-store-module.h"
-#include "ns3/mmwave-mac-scheduler-tdma-rr.h"
-#include "ns3/bandwidth-part-gnb.h"
-#include "ns3/grid-scenario-helper.h"
-#include "ns3/mmwave-enb-phy.h"
-#include "ns3/cc-bwp-helper.h"
 
+/*
+ * To be able to use LOG_* functions.
+ */
+#include "ns3/log.h"
+
+/*
+ * Use, always, the namespace ns3. All the NR classes are inside such namespace.
+ */
 using namespace ns3;
 
+/*
+ * With this line, we will be able to see the logs of the file by enabling the
+ * component "CttcNrDemo", in this way:
+ *
+ * $ export NS_LOG="CttcNrDemo=level_info|prefix_func|prefix_time"
+ */
 NS_LOG_COMPONENT_DEFINE ("CttcNrDemo");
 
 int 
 main (int argc, char *argv[])
 {
-  bool udpFullBuffer = false;
-  int32_t fixedMcs = -1;
+  /*
+   * Variables that represent the parameters we will accept as input by the
+   * command line. Each of them is initialized with a default value.
+   */
+  // Scenario parameters (that we will use inside this script):
   uint16_t gNbNum = 1;
   uint16_t ueNumPergNb = 1;
+  bool logging = false;
+  bool singleBwp = false;
+
+  // Traffic parameters (that we will use inside this script:)
+  bool udpFullBuffer = false;
+  uint32_t udpPacketSizeUll = 100;
+  uint32_t udpPacketSizeBe = 1252;
+  uint32_t lambdaUll = 10000;
+  uint32_t lambdaBe = 10000;
+
+  // Simulation parameters. Please don't use double to indicate seconds, use
+  // milliseconds and integers to avoid representation errors.
+  uint32_t simTimeMs = 1000;
+  uint32_t udpAppStartTimeMs = 400;
+
+  // NR parameters. We will take the input from the command line, and then we
+  // will pass them inside the NR module.
+  int32_t fixedMcs = -1;
   bool cellScan = false;
   double beamSearchAngleStep = 10.0;
   uint16_t numerologyBwp1 = 4;
@@ -58,22 +107,22 @@ main (int argc, char *argv[])
   uint16_t numerologyBwp2 = 2;
   double frequencyBwp2 = 28.2e9;
   double bandwidthBwp2 = 100e6;
-  uint32_t udpPacketSizeUll = 100;
-  uint32_t udpPacketSizeBe = 1252;
-  uint32_t lambdaUll = 10000;
-  uint32_t lambdaBe = 10000;
-  bool singleBwp = false;
+  double totalTxPower = 4;
+
+  // Where we will store the output files.
   std::string simTag = "default";
   std::string outputDir = "./";
-  double totalTxPower = 4;
-  bool logging = false;
 
-  double simTime = 1; // seconds
-  double udpAppStartTime = 0.4; //seconds
-
+  /*
+   * From here, we instruct the ns3::CommandLine class of all the input parameters
+   * that we may accept as input, as well as their description, and the storage
+   * variable.
+   */
   CommandLine cmd;
 
-  cmd.AddValue ("simTime", "Simulation time", simTime);
+  cmd.AddValue ("simTimeMs",
+                "Simulation time",
+                simTimeMs);
   cmd.AddValue ("udpFullBuffer",
                 "Whether to set the full buffer traffic; if this parameter is "
                 "set then the udpInterval parameter will be neglected.",
@@ -144,11 +193,26 @@ main (int argc, char *argv[])
                 logging);
 
 
+  // Parse the command line
   cmd.Parse (argc, argv);
+
+  /*
+   * Check if the frequency is in the allowed range.
+   * If you need to add other checks, here is the best position to put them.
+   */
   NS_ABORT_IF (frequencyBwp1 < 6e9 || frequencyBwp1 > 100e9);
   NS_ABORT_IF (frequencyBwp2 < 6e9 || frequencyBwp2 > 100e9);
 
-  // enable logging or not
+  /*
+   * If the logging variable is set to true, enable the log of some components
+   * through the code. The same effect can be obtained through the use
+   * of the NS_LOG environment variable:
+   *
+   * export NS_LOG="UdpClient=level_info|prefix_time|prefix_func|prefix_node:UdpServer=..."
+   *
+   * Usually, the environment variable way is preferred, as it is more customizable,
+   * and more expressive.
+   */
   if (logging)
     {
       LogComponentEnable ("UdpClient", LOG_LEVEL_INFO);
@@ -156,15 +220,17 @@ main (int argc, char *argv[])
       LogComponentEnable ("LtePdcp", LOG_LEVEL_INFO);
     }
 
-  Config::SetDefault ("ns3::LteRlcUm::MaxTxBufferSize",
-                      UintegerValue(999999999));
+  /*
+   * Default values for the simulation. We are progressively removing all
+   * the instances of SetDefault, but we need it for legacy code (LTE)
+   */
+  Config::SetDefault ("ns3::LteRlcUm::MaxTxBufferSize", UintegerValue(999999999));
 
-  Config::SetDefault ("ns3::PointToPointEpcHelper::S1uLinkDelay", TimeValue (MilliSeconds(0)));
-  Config::SetDefault ("ns3::BwpManagerAlgorithmStatic::NGBR_LOW_LAT_EMBB", UintegerValue (0));
-  Config::SetDefault ("ns3::BwpManagerAlgorithmStatic::GBR_CONV_VOICE", UintegerValue (1));
-  Config::SetDefault ("ns3::IdealBeamformingHelper::IdealBeamformingMethod", TypeIdValue (DirectPathBeamforming::GetTypeId ()));
-
-  // create base stations and mobile terminals
+  /*
+   * Create the scenario. In our examples, we heavily use helpers that setup
+   * the gnbs and ue following a pre-defined pattern. Please have a look at the
+   * GridScenarioHelper documentation to see how the nodes will be distributed.
+   */
   GridScenarioHelper gridScenario;
   gridScenario.SetRows (1);
   gridScenario.SetColumns (gNbNum);
@@ -175,29 +241,57 @@ main (int argc, char *argv[])
   gridScenario.SetUtNumber (ueNumPergNb * gNbNum);
   gridScenario.CreateScenario ();
 
-  // setup the mmWave simulation
+  /*
+   * Setup the NR module. We create the various helpers needed for the
+   * NR simulation:
+   * - EpcHelper, which will setup the core network
+   * - IdealBeamformingHelper, which takes care of the beamforming part
+   * - MmWaveHelper, which takes care of creating and connecting the various
+   * part of the NR stack
+   */
   Ptr<NrPointToPointEpcHelper> epcHelper = CreateObject<NrPointToPointEpcHelper> ();
   Ptr<IdealBeamformingHelper> idealBeamformingHelper = CreateObject<IdealBeamformingHelper>();
   Ptr<MmWaveHelper> mmWaveHelper = CreateObject<MmWaveHelper> ();
-  mmWaveHelper->SetIdealBeamformingHelper(idealBeamformingHelper);
+
+  // Put the pointers inside mmWaveHelper
+  mmWaveHelper->SetIdealBeamformingHelper (idealBeamformingHelper);
   mmWaveHelper->SetEpcHelper (epcHelper);
 
-  double x = pow(10, totalTxPower/10);
-  double totalBandwidth = bandwidthBwp1;
-
+  /*
+   * Spectrum division. We create two operational bands, each of them containing
+   * one bandwidth part centered at the frequency specified by the input parameters.
+   * Each spectrum part length is, as well, specified by the input parameters.
+   * Both operational bands will use the StreetCanyon channel modeling.
+   */
   BandwidthPartInfoPtrVector allBwps;
+  CcBwpCreator ccBwpCreator;
 
+  // Create the configuration for the CcBwpHelper
   CcBwpCreator::SimpleOperationBandConf bandConf1 (frequencyBwp1, bandwidthBwp1, 1, BandwidthPartInfo::UMi_StreetCanyon);
   CcBwpCreator::SimpleOperationBandConf bandConf2 (frequencyBwp2, bandwidthBwp2, 1, BandwidthPartInfo::UMi_StreetCanyon);
 
-  CcBwpCreator ccBwpCreator;
+  // By using the configuration created, it is time to make the operation bands
   OperationBandInfo band1 = ccBwpCreator.CreateOperationBandContiguousCc (bandConf1);
   OperationBandInfo band2 = ccBwpCreator.CreateOperationBandContiguousCc (bandConf2);
 
-  // Initialize channel and pathloss, plus other things inside band1
+  /*
+   * Initialize channel and pathloss, plus other things inside band1. If needed,
+   * the band configuration can be done manually, but we leave it for more
+   * sophisticated examples. For the moment, this method will take care
+   * of all the spectrum initialization needs.
+   */
   mmWaveHelper->InitializeOperationBand (&band1);
 
-  // if not single BWP simulation, initialize and setup power in the second bwp
+  /*
+   * Start to account for the bandwidth used by the example, as well as
+   * the total power that has to be divided among the bwp.
+   */
+  double x = pow (10, totalTxPower/10);
+  double totalBandwidth = bandwidthBwp1;
+
+  /*
+   * if not single BWP simulation, initialize and setup power in the second bwp
+   */
   if (!singleBwp)
     {
       // Initialize channel and pathloss, plus other things inside band2
@@ -210,26 +304,89 @@ main (int argc, char *argv[])
       allBwps = CcBwpCreator::GetAllBwps ({band1});
     }
 
+  /*
+   * allBwps contains all the spectrum configuration needed for the mmWaveHelper.
+   *
+   * Now, we can setup the attributes. We can have three kind of attributes:
+   * (i) parameters that are valid for all the bandwidth parts and applies to
+   * all nodes, (ii) parameters that are valid for all the bandwidth parts
+   * and applies to some node only, and (iii) parameters that are different for
+   * every bandwidth parts. The approach is:
+   *
+   * - for (i): Configure the attribute through the helper, and then install;
+   * - for (ii): Configure the attribute through the helper, and then install
+   * for the first set of nodes. Then, change the attribute through the helper,
+   * and install again;
+   * - for (iii): Install, and then configure the attributes by retrieving
+   * the pointer needed, and calling "SetAttribute" on top of such pointer.
+   *
+   */
+
+  /*
+   *  Case (i): Attributes valid for all the nodes
+   */
+  // Beamforming method
+  idealBeamformingHelper->SetAttribute ("IdealBeamformingMethod", TypeIdValue (DirectPathBeamforming::GetTypeId ()));
+
+  // Core latency
+  epcHelper->SetAttribute ("S1uLinkDelay", TimeValue (MilliSeconds (0)));
+
+  // Antennas for all the UEs
   mmWaveHelper->SetUeAntennaAttribute ("NumRows", UintegerValue (2));
   mmWaveHelper->SetUeAntennaAttribute ("NumColumns", UintegerValue (4));
 
+  // Antennas for all the gNbs
   mmWaveHelper->SetGnbAntennaAttribute ("NumRows", UintegerValue (4));
   mmWaveHelper->SetGnbAntennaAttribute ("NumColumns", UintegerValue (8));
 
-  mmWaveHelper->SetGnbPhyAttribute ("Numerology", UintegerValue (2));
+  // gNb routing between Bearer and bandwidh part
+  mmWaveHelper->SetGnbBwpManagerAlgorithmAttribute ("NGBR_LOW_LAT_EMBB", UintegerValue (0));
+  mmWaveHelper->SetGnbBwpManagerAlgorithmAttribute ("GBR_CONV_VOICE", UintegerValue (1));
 
-  // install mmWave net devices
+  // Ue routing between Bearer and bandwidth part
+  mmWaveHelper->SetUeBwpManagerAlgorithmAttribute ("NGBR_LOW_LAT_EMBB", UintegerValue (0));
+  mmWaveHelper->SetUeBwpManagerAlgorithmAttribute ("GBR_CONV_VOICE", UintegerValue (1));
+
+  /*
+   * We miss many other parameters. By default, not configuring them is equivalent
+   * to use the default values. Please, have a look at the documentation to see
+   * what are the default values for all the attributes you are not seeing here.
+   */
+
+  /*
+   * Case (ii): Attributes valid for a subset of the nodes
+   */
+
+  // NOT PRESENT IN THIS SIMPLE EXAMPLE
+
+  /*
+   * We have configured the attributes we needed. Now, install and get the pointers
+   * to the NetDevices, which contains all the NR stack:
+   */
+
   NetDeviceContainer enbNetDev = mmWaveHelper->InstallGnbDevice (gridScenario.GetBaseStations (), allBwps);
   NetDeviceContainer ueNetDev = mmWaveHelper->InstallUeDevice (gridScenario.GetUserTerminals (), allBwps);
 
-  NS_ASSERT (enbNetDev.GetN () == 1);
+  /*
+   * Case (iii): Go node for node and change the attributes we have to setup
+   * per-node.
+   */
 
-  mmWaveHelper->GetEnbPhy (enbNetDev.Get (0), 0)->SetTxPower (10*log10 ((bandwidthBwp1/totalBandwidth) * x));
+  // Get the first netdevice (enbNetDev.Get (0)) and the first bandwidth part (0)
+  // and set the attribute.
+  mmWaveHelper->GetEnbPhy (enbNetDev.Get (0), 0)->SetAttribute ("Numerology", UintegerValue (numerologyBwp1));
+  mmWaveHelper->GetEnbPhy (enbNetDev.Get (0), 0)->SetAttribute ("TxPower", DoubleValue (10*log10 ((bandwidthBwp1/totalBandwidth) * x)));
 
   if (!singleBwp)
     {
+      // Get the first netdevice (enbNetDev.Get (0)) and the second bandwidth part (1)
+      // and set the attribute.
+      mmWaveHelper->GetEnbPhy (enbNetDev.Get (0), 1)->SetAttribute ("Numerology", UintegerValue (numerologyBwp2));
       mmWaveHelper->GetEnbPhy (enbNetDev.Get (0), 1)->SetTxPower (10*log10 ((bandwidthBwp2/totalBandwidth) * x));
     }
+
+  // From here, it is standard NS3. In the future, we will create helpers
+  // for this part as well.
 
   // create the internet and install the IP stack on the UEs
   // get SGW/PGW and create a single RemoteHost
@@ -340,10 +497,10 @@ main (int argc, char *argv[])
       mmWaveHelper->ActivateDedicatedEpsBearer(ueNetDev.Get(j), bearer, tft);
     }
   // start UDP server and client apps
-  serverApps.Start(Seconds(udpAppStartTime));
-  clientApps.Start(Seconds(udpAppStartTime));
-  serverApps.Stop(Seconds(simTime));
-  clientApps.Stop(Seconds(simTime));
+  serverApps.Start(MilliSeconds(udpAppStartTimeMs));
+  clientApps.Start(MilliSeconds(udpAppStartTimeMs));
+  serverApps.Stop(MilliSeconds(simTimeMs));
+  clientApps.Stop(MilliSeconds(simTimeMs));
 
   // enable the traces provided by the mmWave module
   //mmWaveHelper->EnableTraces();
@@ -359,7 +516,7 @@ main (int argc, char *argv[])
   monitor->SetAttribute ("JitterBinWidth", DoubleValue (0.001));
   monitor->SetAttribute ("PacketSizeBinWidth", DoubleValue (20));
 
-  Simulator::Stop (Seconds (simTime));
+  Simulator::Stop (MilliSeconds (simTimeMs));
   Simulator::Run ();
 
   /*
@@ -404,13 +561,13 @@ main (int argc, char *argv[])
       outFile << "Flow " << i->first << " (" << t.sourceAddress << ":" << t.sourcePort << " -> " << t.destinationAddress << ":" << t.destinationPort << ") proto " << protoStream.str () << "\n";
       outFile << "  Tx Packets: " << i->second.txPackets << "\n";
       outFile << "  Tx Bytes:   " << i->second.txBytes << "\n";
-      outFile << "  TxOffered:  " << i->second.txBytes * 8.0 / (simTime - udpAppStartTime) / 1000 / 1000  << " Mbps\n";
+      outFile << "  TxOffered:  " << i->second.txBytes * 8.0 / ((simTimeMs - udpAppStartTimeMs) / 1000.0) / 1000.0 / 1000.0  << " Mbps\n";
       outFile << "  Rx Bytes:   " << i->second.rxBytes << "\n";
       if (i->second.rxPackets > 0)
         {
           // Measure the duration of the flow from receiver's perspective
           //double rxDuration = i->second.timeLastRxPacket.GetSeconds () - i->second.timeFirstTxPacket.GetSeconds ();
-          double rxDuration = (simTime - udpAppStartTime);
+          double rxDuration = (simTimeMs - udpAppStartTimeMs) / 1000.0;
 
           averageFlowThroughput += i->second.rxBytes * 8.0 / rxDuration / 1000 / 1000;
           averageFlowDelay += 1000 * i->second.delaySum.GetSeconds () / i->second.rxPackets;
