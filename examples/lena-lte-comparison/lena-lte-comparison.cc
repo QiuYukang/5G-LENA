d3c2127fca25c6365458e1cfb0c03383da5ad098
bbojovic <biljana.bojovic@cttc.es>
2020-05-18 18:07:19 +0200

    lena-lte-comparison: add conf to calculate UL CQI based on PUSCH

--- examples/lena-lte-comparison/lena-lte-comparison.cc
+++ examples/lena-lte-comparison/lena-lte-comparison.cc
@@ -213,6 +213,7 @@ void SetLenaSimulatorParameters (HexagonalGridScenarioHelper gridScenario,
   lteHelper->SetEpcHelper (epcHelper);
 
   // ALL SECTORS AND BANDS configuration
+  Config::SetDefault ("ns3::FfMacScheduler::UlCqiFilter", EnumValue (FfMacScheduler::PUSCH_UL_CQI));
   Config::SetDefault ("ns3::LteSpectrumPhy::CtrlErrorModelEnabled", BooleanValue (false));
   Config::SetDefault ("ns3::LteRlcUm::MaxTxBufferSize", UintegerValue(999999999));
   Config::SetDefault ("ns3::LteGnbPhy::TxPower", DoubleValue (txPower));
