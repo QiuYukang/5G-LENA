fd945f86ea03f90cddbb60fb410085f4bfa7fc5b
Nat <natale.patriciello@gmail.com>
2020-06-01 09:37:57 +0200

    test: split sched-tdma-rr in multuple sub-tests

--- test/nr-system-test-schedulers-tdma-rr.cc
+++ test/nr-system-test-schedulers-tdma-rr.cc
@@ -36,33 +36,26 @@ using namespace ns3;
  *
  * It will check Tdma RR with:
  *
- * - DL, UL, DL and UL together
+ * - DL
  * - UEs per beam: 1, 2, 4, 8
  * - beams: 1, 2
  * - numerologies: 0, 1
  */
-class NrSystemTestSchedulerTdmaRrSuite : public TestSuite
+class NrSystemTestSchedulerTdmaRrDlSuite : public TestSuite
 {
 public:
   /**
    * \brief constructor
    */
-  NrSystemTestSchedulerTdmaRrSuite ();
+  NrSystemTestSchedulerTdmaRrDlSuite ();
 };
 
-NrSystemTestSchedulerTdmaRrSuite::NrSystemTestSchedulerTdmaRrSuite ()
-  : TestSuite ("nr-system-test-schedulers-tdma-rr", SYSTEM)
+NrSystemTestSchedulerTdmaRrDlSuite::NrSystemTestSchedulerTdmaRrDlSuite ()
+  : TestSuite ("nr-system-test-schedulers-tdma-rr-dl", SYSTEM)
 {
-  enum TxMode
-  {
-    DL,
-    UL,
-    DL_UL
-  };
-
   std::list<std::string> subdivision     = {"Tdma",};
   std::list<std::string> scheds          = {"RR"};
-  std::list<TxMode>      mode            = {DL, UL, DL_UL};
+  std::list<std::string> mode            = {"DL",};
   std::list<uint32_t>    uesPerBeamList  = {1, 2, 4, 8};
   std::list<uint32_t>    beams           = {1, 2};
   std::list<uint32_t>    numerologies    = {0, 1, }; // Test only num 0 and 1
@@ -80,28 +73,16 @@ NrSystemTestSchedulerTdmaRrSuite::NrSystemTestSchedulerTdmaRrSuite ()
                       for (const auto & beam : beams)
                         {
                           std::stringstream ss, schedName;
-                          if (modeType == DL)
-                            {
-                              ss << "DL";
-                            }
-                          else if (modeType == UL)
-                            {
-                              ss << "UL";
-                            }
-                          else
-                            {
-                              ss << "DL_UL";
-                            }
-                          ss << ", Num " << num << ", " << subType << " " << sched << ", "
+                          ss << ", Num " << num << ", " << modeType << ", " << subType << " " << sched << ", "
                              << uesPerBeam << " UE per beam, " << beam << " beam";
-                          const bool isDl = modeType == DL || modeType == DL_UL;
-                          const bool isUl = modeType == UL || modeType == DL_UL;
+                          const bool isDl = modeType == "DL" || modeType == "DL_UL";
+                          const bool isUl = modeType == "UL" || modeType == "DL_UL";
 
                           schedName << "ns3::NrMacScheduler" << subType << sched;
 
                           AddTestCase (new SystemSchedulerTest (ss.str(), uesPerBeam, beam, num,
-                                                                       20e6, isDl, isUl,
-                                                                       schedName.str()),
+                                                                20e6, isDl, isUl,
+                                                                schedName.str()),
                                        TestCase::QUICK);
                         }
                     }
@@ -111,6 +92,136 @@ NrSystemTestSchedulerTdmaRrSuite::NrSystemTestSchedulerTdmaRrSuite ()
     }
 }
 
-static NrSystemTestSchedulerTdmaRrSuite schedTdmaRrSuite;
+static NrSystemTestSchedulerTdmaRrDlSuite nrSystemTestSchedulerTdmaRrDlSuite;
 
+// ----------------------------------------------------------------------------
 
+/**
+ * \brief The TDMA RR scheduler system test suite
+ *
+ * It will check Tdma RR with:
+ *
+ * - UL
+ * - UEs per beam: 1, 2, 4, 8
+ * - beams: 1, 2
+ * - numerologies: 0, 1
+ */
+class NrSystemTestSchedulerTdmaRrUlSuite : public TestSuite
+{
+public:
+  /**
+   * \brief constructor
+   */
+  NrSystemTestSchedulerTdmaRrUlSuite ();
+};
+
+NrSystemTestSchedulerTdmaRrUlSuite::NrSystemTestSchedulerTdmaRrUlSuite ()
+  : TestSuite ("nr-system-test-schedulers-tdma-rr-ul", SYSTEM)
+{
+  std::list<std::string> subdivision     = {"Tdma",};
+  std::list<std::string> scheds          = {"RR"};
+  std::list<std::string> mode            = {"UL",};
+  std::list<uint32_t>    uesPerBeamList  = {1, 2, 4, 8};
+  std::list<uint32_t>    beams           = {1, 2};
+  std::list<uint32_t>    numerologies    = {0, 1, }; // Test only num 0 and 1
+
+  for (const auto & num : numerologies)
+    {
+      for (const auto & subType : subdivision)
+        {
+          for (const auto & sched : scheds)
+            {
+              for (const auto & modeType : mode)
+                {
+                  for (const auto & uesPerBeam : uesPerBeamList)
+                    {
+                      for (const auto & beam : beams)
+                        {
+                          std::stringstream ss, schedName;
+                          ss << ", Num " << num << ", " << modeType << ", " << subType << " " << sched << ", "
+                             << uesPerBeam << " UE per beam, " << beam << " beam";
+                          const bool isDl = modeType == "DL" || modeType == "DL_UL";
+                          const bool isUl = modeType == "UL" || modeType == "DL_UL";
+
+                          schedName << "ns3::NrMacScheduler" << subType << sched;
+
+                          AddTestCase (new SystemSchedulerTest (ss.str(), uesPerBeam, beam, num,
+                                                                20e6, isDl, isUl,
+                                                                schedName.str()),
+                                       TestCase::QUICK);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+static NrSystemTestSchedulerTdmaRrUlSuite nrSystemTestSchedulerTdmaRrUlSuite;
+
+// ----------------------------------------------------------------------------
+
+/**
+ * \brief The TDMA RR scheduler system test suite
+ *
+ * It will check Tdma RR with:
+ *
+ * - DL/UL
+ * - UEs per beam: 1, 2, 4, 8
+ * - beams: 1, 2
+ * - numerologies: 0, 1
+ */
+class NrSystemTestSchedulerTdmaRrDlUlSuite : public TestSuite
+{
+public:
+  /**
+   * \brief constructor
+   */
+  NrSystemTestSchedulerTdmaRrDlUlSuite ();
+};
+
+NrSystemTestSchedulerTdmaRrDlUlSuite::NrSystemTestSchedulerTdmaRrDlUlSuite ()
+  : TestSuite ("nr-system-test-schedulers-tdma-rr-dl-ul", SYSTEM)
+{
+  std::list<std::string> subdivision     = {"Tdma",};
+  std::list<std::string> scheds          = {"RR"};
+  std::list<std::string> mode            = {"DL_UL",};
+  std::list<uint32_t>    uesPerBeamList  = {8};
+  std::list<uint32_t>    beams           = {2,};
+  std::list<uint32_t>    numerologies    = {0, }; // Test only num 0 and 1
+
+  for (const auto & num : numerologies)
+    {
+      for (const auto & subType : subdivision)
+        {
+          for (const auto & sched : scheds)
+            {
+              for (const auto & modeType : mode)
+                {
+                  for (const auto & uesPerBeam : uesPerBeamList)
+                    {
+                      for (const auto & beam : beams)
+                        {
+                          std::stringstream ss, schedName;
+                          ss << ", Num " << num << ", " << modeType << ", " << subType << " " << sched << ", "
+                             << uesPerBeam << " UE per beam, " << beam << " beam";
+                          const bool isDl = modeType == "DL" || modeType == "DL_UL";
+                          const bool isUl = modeType == "UL" || modeType == "DL_UL";
+
+                          schedName << "ns3::NrMacScheduler" << subType << sched;
+
+                          AddTestCase (new SystemSchedulerTest (ss.str(), uesPerBeam, beam, num,
+                                                                20e6, isDl, isUl,
+                                                                schedName.str()),
+                                       TestCase::QUICK);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+static NrSystemTestSchedulerTdmaRrDlUlSuite nrSystemTestSchedulerTdmaRrDlUlSuite;
+
+// ----------------------------------------------------------------------------
