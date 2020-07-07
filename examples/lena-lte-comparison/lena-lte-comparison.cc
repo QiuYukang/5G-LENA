3ab529320ebcb0573f98ab1dd39e869f675c37c8
Nat <natale.patriciello@gmail.com>
2020-07-07 11:31:35 +0200

    lena-lte-comp: UE noise figure to 9.0

--- examples/lena-lte-comparison/lena-lte-comparison.cc
+++ examples/lena-lte-comparison/lena-lte-comparison.cc
@@ -232,7 +232,7 @@ void SetLenaSimulatorParameters (HexagonalGridScenarioHelper gridScenario,
   Config::SetDefault ("ns3::LteRlcUm::MaxTxBufferSize", UintegerValue(999999999));
   Config::SetDefault ("ns3::LteEnbPhy::TxPower", DoubleValue (txPower));
   Config::SetDefault ("ns3::LteUePhy::TxPower", DoubleValue (ueTxPower));
-  Config::SetDefault ("ns3::LteUePhy::NoiseFigure", DoubleValue (5.0));
+  Config::SetDefault ("ns3::LteUePhy::NoiseFigure", DoubleValue (9.0));
   Config::SetDefault ("ns3::LteUePhy::EnableRlfDetection", BooleanValue (false));
   Config::SetDefault ("ns3::LteAmc::AmcModel", EnumValue(LteAmc::PiroEW2010));
   lteHelper->SetAttribute ("PathlossModel", StringValue (pathlossModel)); // for each band the same pathloss model
@@ -475,6 +475,9 @@ void Set5gLenaSimulatorParameters (const HexagonalGridScenarioHelper &gridScenar
   Config::SetDefault ("ns3::ThreeGppChannelModel::UpdatePeriod",TimeValue (MilliSeconds(100)));
   nrHelper->SetChannelConditionModelAttribute ("UpdatePeriod", TimeValue (MilliSeconds (0)));
   nrHelper->SetPathlossAttribute ("ShadowingEnabled", BooleanValue (false));
+
+  // Noise figure for the UE
+  nrHelper->SetUePhyAttribute ("NoiseFigure", DoubleValue (9.0));
 
   // Error Model: UE and GNB with same spectrum error model.
   nrHelper->SetUlErrorModel (errorModel);
