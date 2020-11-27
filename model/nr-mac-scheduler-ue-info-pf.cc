b07b2b54ce71e4ac2870803ce1555104eb695f32
kkoutlia <katerina.koutlia@cttc.es>
2020-11-27 11:20:18 +0100

    sched-ue-info-pf: Prepare functions for the UL

--- model/nr-mac-scheduler-ue-info-pf.cc
+++ model/nr-mac-scheduler-ue-info-pf.cc
@@ -25,7 +25,7 @@ namespace ns3 {
 NS_LOG_COMPONENT_DEFINE ("NrMacSchedulerUeInfoPF");
 
 /**
- * \brief Update PF metrics
+ * \brief Update DL PF metrics
  * \param timeWindow Time window to consider
  * \param config Config
  * \param amc AMC
@@ -39,17 +39,49 @@ NrMacSchedulerUeInfoPF::UpdateDlPFMetric (const NrMacSchedulerNs3::FTResources &
                                               const Ptr<const NrAmc> &amc)
 {
   NS_LOG_FUNCTION (this);
+
   NrMacSchedulerUeInfo::UpdateDlMetric (amc);
 
   m_currTputDl = static_cast<double> (m_dlTbSize) / (totAssigned.m_sym);
   m_avgTputDl = ((1.0 - (1.0 / static_cast<double> (timeWindow))) * m_lastAvgTputDl) +
     ((1.0 / timeWindow) * m_currTputDl);
-  NS_LOG_DEBUG ("Update PF Metric for UE " << static_cast<uint32_t> (m_rnti) <<
-                " TBS: " << m_dlTbSize << " Updated currTput " <<
-                m_currTputDl << " avgTput " << m_avgTputDl << " over n. of syms: " <<
-                +totAssigned.m_sym <<  ", last Avg TH " <<
-                m_lastAvgTputDl << " total sym assigned " << static_cast<uint32_t> (totAssigned.m_sym) <<
-                " updated metric: " << m_potentialTput / std::max (1E-9, m_avgTputDl));
+
+  NS_LOG_DEBUG ("Update DL PF Metric for UE " << static_cast<uint32_t> (m_rnti) <<
+                " DL TBS: " << m_dlTbSize << " Updated currTputDl " <<
+                m_currTputDl << " avgTputDl " << m_avgTputDl << " over n. of syms: " <<
+                +totAssigned.m_sym <<  ", last Avg TH Dl" << m_lastAvgTputDl <<
+                " total sym assigned " << static_cast<uint32_t> (totAssigned.m_sym) <<
+                " updated DL metric: " << m_potentialTput / std::max (1E-9, m_avgTputDl));
+}
+
+/**
+ * \brief Update UL PF metrics
+ * \param timeWindow Time window to consider
+ * \param config Config
+ * \param amc AMC
+ *
+ * Updates m_currTputUl and m_avgTputUl by keeping in consideration
+ * the assigned resources (in form of TBS) and the time window.
+ */
+void
+NrMacSchedulerUeInfoPF::UpdateUlPFMetric (const NrMacSchedulerNs3::FTResources &totAssigned,
+                                              double timeWindow,
+                                              const Ptr<const NrAmc> &amc)
+{
+  NS_LOG_FUNCTION (this);
+
+  NrMacSchedulerUeInfo::UpdateUlMetric (amc);
+
+  m_currTputUl = static_cast<double> (m_ulTbSize) / (totAssigned.m_sym);
+  m_avgTputUl = ((1.0 - (1.0 / static_cast<double> (timeWindow))) * m_lastAvgTputUl) +
+    ((1.0 / timeWindow) * m_currTputUl);
+
+  NS_LOG_DEBUG ("Update UL PF Metric for UE " << static_cast<uint32_t> (m_rnti) <<
+                " UL TBS: " << m_ulTbSize << " Updated currTputUl " <<
+                m_currTputUl << " avgTputUl " << m_avgTputUl << " over n. of syms: " <<
+                +totAssigned.m_sym <<  ", last Avg TH Ul" << m_lastAvgTputUl <<
+                " total sym assigned " << static_cast<uint32_t> (totAssigned.m_sym) <<
+                " updated UL metric: " << m_potentialTputUl / std::max (1E-9, m_avgTputUl));
 }
 
 void
@@ -57,11 +89,29 @@ NrMacSchedulerUeInfoPF::CalculatePotentialTPut (const NrMacSchedulerNs3::FTResou
                                                     const Ptr<const NrAmc> &amc)
 {
   NS_LOG_FUNCTION (this);
+
   uint32_t rbsAssignable = assignableInIteration.m_rbg * GetNumRbPerRbg ();
   m_potentialTput =  amc->CalculateTbSize (m_dlMcs, rbsAssignable);
   m_potentialTput /= assignableInIteration.m_sym;
-  NS_LOG_INFO ("UE " << m_rnti << " potentialTput " << m_potentialTput <<
-               " lastAvgTh " << m_lastAvgTputDl << " metric: " << m_potentialTput / std::max (1E-9, m_avgTputDl));
+
+  NS_LOG_INFO ("UE " << m_rnti << " potentialTputDl " << m_potentialTput <<
+               " lastAvgThDl " << m_lastAvgTputDl <<
+               " DL metric: " << m_potentialTput / std::max (1E-9, m_avgTputDl));
+}
+
+void
+NrMacSchedulerUeInfoPF::CalculatePotentialTPutUl (const NrMacSchedulerNs3::FTResources &assignableInIteration,
+                                                    const Ptr<const NrAmc> &amc)
+{
+  NS_LOG_FUNCTION (this);
+
+  uint32_t rbsAssignable = assignableInIteration.m_rbg * GetNumRbPerRbg ();
+  m_potentialTputUl =  amc->CalculateTbSize (m_ulMcs, rbsAssignable);
+  m_potentialTputUl /= assignableInIteration.m_sym;
+
+  NS_LOG_INFO ("UE " << m_rnti << " potentialTputUl " << m_potentialTputUl <<
+               " lastAvgThUl " << m_lastAvgTputUl <<
+               " UL metric: " << m_potentialTputUl / std::max (1E-9, m_avgTputUl));
 }
 
 } // namespace ns3
--- model/nr-mac-scheduler-ue-info-pf.h
+++ model/nr-mac-scheduler-ue-info-pf.h
@@ -31,6 +31,7 @@ namespace ns3 {
  * to sort the UEs in case of a PF scheduler.
  *
  * \see CompareUeWeightsDl
+ * \see CompareUeWeightsUl
  */
 class NrMacSchedulerUeInfoPF : public NrMacSchedulerUeInfo
 {
@@ -48,7 +49,7 @@ public:
   }
 
   /**
-   * \brief Reset PF scheduler info
+   * \brief Reset DL PF scheduler info
    *
    * Set the last average throughput to the current average throughput,
    * and zeroes the average throughput as well as the current throughput.
@@ -65,12 +66,38 @@ public:
   }
 
   /**
-   * \brief Reset the avg Th to the last value
+   * \brief Reset UL PF scheduler info
+   *
+   * Set the last average throughput to the current average throughput,
+   * and zeroes the average throughput as well as the current throughput.
+   *
+   * It calls also NrMacSchedulerUeInfo::ResetUlSchedInfo.
+   */
+  virtual void ResetUlSchedInfo () override
+  {
+    m_lastAvgTputUl = m_avgTputUl;
+    m_avgTputUl = 0.0;
+    m_currTputUl = 0.0;
+    m_potentialTputUl = 0.0;
+    NrMacSchedulerUeInfo::ResetUlSchedInfo ();
+  }
+
+  /**
+   * \brief Reset the DL avg Th to the last value
    */
   virtual void ResetDlMetric () override
   {
     NrMacSchedulerUeInfo::ResetDlMetric ();
     m_avgTputDl = m_lastAvgTputDl;
+  }
+
+  /**
+   * \brief Reset the UL avg Th to the last value
+   */
+  virtual void ResetUlMetric () override
+  {
+    NrMacSchedulerUeInfo::ResetUlMetric ();
+    m_avgTputUl = m_lastAvgTputUl;
   }
 
   /**
@@ -82,13 +109,33 @@ public:
   void UpdateDlPFMetric (const NrMacSchedulerNs3::FTResources &totAssigned,
                          double timeWindow,
                          const Ptr<const NrAmc> &amc);
+
   /**
-   * \brief Calculate the Potential throughput
+   * \brief Update the PF metric for uplink
+   * \param totAssigned the resources assigned
+   * \param timeWindow the time window
+   * \param amc a pointer to the AMC
+   */
+  void UpdateUlPFMetric (const NrMacSchedulerNs3::FTResources &totAssigned,
+                         double timeWindow,
+                         const Ptr<const NrAmc> &amc);
+
+  /**
+   * \brief Calculate the Potential throughput for downlink
    * \param assignableInIteration resources assignable
    * \param amc a pointer to the AMC
    */
   void CalculatePotentialTPut (const NrMacSchedulerNs3::FTResources &assignableInIteration,
                                const Ptr<const NrAmc> &amc);
+
+  /**
+   * \brief Calculate the Potential throughput for uplink
+   * \param assignableInIteration resources assignable
+   * \param amc a pointer to the AMC
+   */
+  void CalculatePotentialTPutUl (const NrMacSchedulerNs3::FTResources &assignableInIteration,
+                               const Ptr<const NrAmc> &amc);
+
 
   /**
    * \brief comparison function object (i.e. an object that satisfies the
@@ -117,11 +164,44 @@ public:
     return (lPfMetric > rPfMetric);
   }
 
+  /**
+   * \brief comparison function object (i.e. an object that satisfies the
+   * requirements of Compare) which returns ​true if the first argument is less
+   * than (i.e. is ordered before) the second.
+   * \param lue Left UE
+   * \param rue Right UE
+   * \return true if the PF metric of the left UE are higher than the right UE
+   *
+   * The PF metric is calculated as following:
+   *
+   * \f$ pfMetric_{i} = std::pow(potentialTPut_{i}, alpha) / std::max (1E-9, m_avgTput_{i}) \f$
+   *
+   * Alpha is a fairness metric. Please note that the throughput is calculated
+   * in bit/symbol.
+   */
+  static bool CompareUeWeightsUl (const NrMacSchedulerNs3::UePtrAndBufferReq &lue,
+                                  const NrMacSchedulerNs3::UePtrAndBufferReq &rue)
+  {
+    auto luePtr = dynamic_cast<NrMacSchedulerUeInfoPF*> (lue.first.get ());
+    auto ruePtr = dynamic_cast<NrMacSchedulerUeInfoPF*> (rue.first.get ());
+
+    double lPfMetric = std::pow (luePtr->m_potentialTputUl, luePtr->m_alpha) / std::max (1E-9, luePtr->m_avgTputUl);
+    double rPfMetric = std::pow (ruePtr->m_potentialTputUl, ruePtr->m_alpha) / std::max (1E-9, ruePtr->m_avgTputUl);
+
+    return (lPfMetric > rPfMetric);
+  }
+
   double m_currTputDl {0.0};    //!< Current slot throughput
   double m_avgTputDl  {0.0};    //!< Average throughput during all the slots
   double m_lastAvgTputDl {0.0}; //!< Last average throughput
   double m_potentialTput {0.0}; //!< Potential throughput in one assignable resource (can be a symbol or a RBG)
   float  m_alpha {0.0};         //!< PF fairness metric
+
+  double m_currTputUl {0.0};    //!< Current slot throughput
+  double m_avgTputUl  {0.0};    //!< Average throughput during all the slots
+  double m_lastAvgTputUl {0.0}; //!< Last average throughput
+  double m_potentialTputUl {0.0}; //!< Potential throughput in one assignable resource (can be a symbol or a RBG)
 };
+
 
 } // namespace ns3
