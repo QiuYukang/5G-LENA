35d977b31feb602969b38ff5e0f070af9e20240b
kkoutlia <katerina.koutlia@cttc.es>
2023-02-22 14:30:47 +0100

    lc-assignment: Add new algorithm for DL in LC-QoS

--- model/nr-mac-scheduler-lc-qos.cc
+++ model/nr-mac-scheduler-lc-qos.cc
@@ -5,22 +5,26 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #include "nr-mac-scheduler-lc-qos.h"
+
+#include "ns3/ff-mac-common.h"
 #include "ns3/log.h"
 
+#include <algorithm>
+#include <iostream>
 
-namespace ns3 {
+namespace ns3
+{
 
-NS_LOG_COMPONENT_DEFINE ("NrMacSchedulerLcQos");
-NS_OBJECT_ENSURE_REGISTERED (NrMacSchedulerLcQos);
-
+NS_LOG_COMPONENT_DEFINE("NrMacSchedulerLcQos");
+NS_OBJECT_ENSURE_REGISTERED(NrMacSchedulerLcQos);
 
 NrMacSchedulerLcQos::NrMacSchedulerLcQos()
     : NrMacSchedulerLcAlgorithm()
 {
-    NS_LOG_FUNCTION (this);
+    NS_LOG_FUNCTION(this);
 }
 
-NrMacSchedulerLcQos::~NrMacSchedulerLcQos ()
+NrMacSchedulerLcQos::~NrMacSchedulerLcQos()
 {
 }
 
@@ -41,7 +45,8 @@ NrMacSchedulerLcQos::GetInstanceTypeId() const
 
 std::vector<NrMacSchedulerLcAlgorithm::Assignation>
 NrMacSchedulerLcQos::AssignBytesToDlLC(const std::unordered_map<uint8_t, LCGPtr>& ueLCG,
-                                       uint32_t tbs, [[maybe_unused]]Time slotPeriod) const
+                                       uint32_t tbs,
+                                       Time slotPeriod) const
 {
     NS_LOG_FUNCTION(this);
     GetFirst GetLCGID;
@@ -52,40 +57,136 @@ NrMacSchedulerLcQos::AssignBytesToDlLC(const std::unordered_map<uint8_t, LCGPtr>
     NS_LOG_INFO("To distribute: " << tbs << " bytes over " << ueLCG.size() << " LCG"
                                   << " in Qos manner");
 
-    uint32_t activeLc = 0;
+    uint32_t bytesLeftToBeAssigned = tbs;
+    uint32_t bytesAssigned = 0;
+    uint64_t sumErabGueanteedBitRate = 0;
+
+    std::vector<std::pair<uint8_t, uint8_t>>
+        gbrActiveLCs; // vector that stores the LCG ID and the LC ID of the gbr active LCs
+    std::vector<std::pair<uint8_t, uint8_t>>
+        restActiveLCs; // vector that stores the LCG ID and the LC ID of the rest active LCs
+
+    std::vector<std::pair<uint8_t, uint32_t>>
+        AssignedBytesToGbrLCsList; // vector that stores the LC ID with the bytes assigned for the
+                                   // case of GBR LCs
+
     for (const auto& lcg : ueLCG)
     {
-        std::vector<uint8_t> lcs = GetLCG(lcg)->GetLCId();
-        for (const auto& lcId : lcs)
+        std::vector<uint8_t> ueActiveLCs = GetLCG(lcg)->GetActiveLCIds();
+        for (const auto lcId : ueActiveLCs)
         {
-            if (GetLCG(lcg)->GetTotalSizeOfLC(lcId) > 0)
+            if ((GetLCG(lcg)->GetLC(lcId)->m_resourceType ==
+                     LogicalChannelConfigListElement_s::QBT_DGBR ||
+                 GetLCG(lcg)->GetLC(lcId)->m_resourceType ==
+                     LogicalChannelConfigListElement_s::QBT_GBR) &&
+                GetLCG(lcg)->GetLC(lcId)->m_eRabGuaranteedBitrateDl != UINT64_MAX)
             {
-                ++activeLc;
+                gbrActiveLCs.emplace_back(std::make_pair(GetLCGID(lcg), lcId));
+                sumErabGueanteedBitRate +=
+                    (GetLCG(lcg)->GetLC(lcId)->m_eRabGuaranteedBitrateDl / 8);
+            }
+            restActiveLCs.emplace_back(std::make_pair(GetLCGID(lcg), lcId));
+        }
+    }
+
+    if (gbrActiveLCs.size() > 1 && sumErabGueanteedBitRate >= tbs)
+    {
+        uint32_t bytesPerLcGbr;
+
+        if (bytesLeftToBeAssigned > 0)
+        {
+            NS_ASSERT(gbrActiveLCs.size() != 0);
+            bytesPerLcGbr = bytesLeftToBeAssigned / gbrActiveLCs.size();
+
+            for (const auto& lcg : ueLCG)
+            {
+                for (auto& itGbrActiveLCs : gbrActiveLCs)
+                {
+                    if (itGbrActiveLCs.first == GetLCGID(lcg))
+                    {
+                        AssignedBytesToGbrLCsList.emplace_back(
+                            std::make_pair(itGbrActiveLCs.second, bytesPerLcGbr));
+                    }
+                }
+            }
+            bytesLeftToBeAssigned = 0;
+        }
+    }
+    else if (gbrActiveLCs.size() > 0)
+    {
+        for (const auto& lcg : ueLCG)
+        {
+            for (auto& itGbrActiveLCs : gbrActiveLCs)
+            {
+                if (itGbrActiveLCs.first == GetLCGID(lcg))
+                {
+                    NS_ASSERT_MSG(
+                        GetLCG(lcg)->GetLC(itGbrActiveLCs.second)->m_eRabGuaranteedBitrateDl !=
+                            UINT64_MAX,
+                        "LC is not guaranteed bit rate!");
+
+                    uint32_t bytes = std::min(
+                        static_cast<uint32_t>(
+                            slotPeriod.GetSeconds() *
+                            (GetLCG(lcg)->GetLC(itGbrActiveLCs.second)->m_eRabGuaranteedBitrateDl /
+                             8)),
+                        GetLCG(lcg)->GetLC(itGbrActiveLCs.second)->GetTotalSize());
+
+                    bytesAssigned = bytes >= bytesLeftToBeAssigned ? bytesLeftToBeAssigned : bytes;
+
+                    AssignedBytesToGbrLCsList.emplace_back(
+                        std::make_pair(itGbrActiveLCs.second, bytesAssigned));
+
+                    NS_ASSERT(bytesLeftToBeAssigned >= bytesAssigned); // check
+                    bytesLeftToBeAssigned -= bytesAssigned;
+                }
             }
         }
     }
 
-    if (activeLc == 0)
-    {
-        return ret;
-    }
+    uint32_t bytesPerLc;
 
-    uint32_t amountPerLC = tbs / activeLc;
-    NS_LOG_INFO("Total LC: " << activeLc << " each one will receive " << amountPerLC << " bytes");
-
-    for (const auto& lcg : ueLCG)
+    if (restActiveLCs.size() != 0 && bytesLeftToBeAssigned > 0)
     {
-        std::vector<uint8_t> lcs = GetLCG(lcg)->GetLCId();
-        for (const auto& lcId : lcs)
+        NS_ASSERT(restActiveLCs.size() != 0);
+        bytesPerLc = bytesLeftToBeAssigned / restActiveLCs.size();
+
+        for (const auto& lcg : ueLCG)
         {
-            if (GetLCG(lcg)->GetTotalSizeOfLC(lcId) > 0)
+            for (auto& itRestActiveLCs : restActiveLCs)
             {
-                NS_LOG_INFO("Assigned to LCID " << static_cast<uint32_t>(lcId) << " inside LCG "
-                                                << static_cast<uint32_t>(GetLCGID(lcg))
-                                                << " an amount of " << amountPerLC << " B");
-                ret.emplace_back(Assignation(GetLCGID(lcg), lcId, amountPerLC));
+                if (itRestActiveLCs.first == GetLCGID(lcg))
+                {
+                    bool erabGbrTrue = false;
+
+                    for (auto it = AssignedBytesToGbrLCsList.begin();
+                         it != AssignedBytesToGbrLCsList.end();
+                         it++)
+                    {
+                        if (it->first == itRestActiveLCs.second && GetLCGID(lcg) == 1)
+                        {
+                            it->second += bytesPerLc;
+                            erabGbrTrue = true;
+                            break;
+                        }
+                    }
+
+                    if (erabGbrTrue == false)
+                    {
+                        NS_LOG_DEBUG("LC : " << +itRestActiveLCs.second
+                                             << " bytes: " << bytesPerLc);
+                        ret.emplace_back(
+                            Assignation(GetLCGID(lcg), itRestActiveLCs.second, bytesPerLc));
+                    }
+                }
             }
         }
+    }
+
+    for (auto& it : AssignedBytesToGbrLCsList)
+    {
+        NS_LOG_DEBUG("LC : " << +it.first << " bytes: " << it.second);
+        ret.emplace_back(Assignation(1, it.first, it.second));
     }
 
     return ret;
@@ -93,16 +194,13 @@ NrMacSchedulerLcQos::AssignBytesToDlLC(const std::unordered_map<uint8_t, LCGPtr>
 
 std::vector<NrMacSchedulerLcAlgorithm::Assignation>
 NrMacSchedulerLcQos::AssignBytesToUlLC(const std::unordered_map<uint8_t, LCGPtr>& ueLCG,
-                                    uint32_t tbs) const
+                                       uint32_t tbs) const
 {
     NS_LOG_FUNCTION(this);
     GetFirst GetLCGID;
     GetSecond GetLCG;
 
     std::vector<NrMacSchedulerLcAlgorithm::Assignation> ret;
-
-    NS_LOG_INFO("To distribute: " << tbs << " bytes over " << ueLCG.size() << " LCG"
-                                  << " in Qos manner");
 
     uint32_t activeLc = 0;
     for (const auto& lcg : ueLCG)
@@ -143,4 +241,4 @@ NrMacSchedulerLcQos::AssignBytesToUlLC(const std::unordered_map<uint8_t, LCGPtr>
     return ret;
 }
 
-}
+} // namespace ns3
--- model/nr-mac-scheduler-lc-qos.h
+++ model/nr-mac-scheduler-lc-qos.h
@@ -16,7 +16,7 @@ namespace ns3
  *\ingroup scheduler
  *
  * \brief Algorithm for distributing the assigned bytes to the different
- * LCGs/LCs of a UE based on the resource type
+ * LCGs/LCs of a UE based on the resource type and the ERAB guaranteed bit rate
  *
  */
 class NrMacSchedulerLcQos : public NrMacSchedulerLcAlgorithm
@@ -26,7 +26,7 @@ class NrMacSchedulerLcQos : public NrMacSchedulerLcAlgorithm
      * \brief Get the type ID.
      * \return the object TypeId
      */
-    static TypeId GetTypeId ();
+    static TypeId GetTypeId();
 
     /**
      * \brief Get the type ID of this instance
@@ -37,23 +37,37 @@ class NrMacSchedulerLcQos : public NrMacSchedulerLcAlgorithm
     /**
      * \brief NrMacSchedulerLcQos constructor
      */
-    NrMacSchedulerLcQos ();
+    NrMacSchedulerLcQos();
     /**
      * \brief NrMacSchedulerLcQos deconstructor
      */
-    ~NrMacSchedulerLcQos () override;
-
+    ~NrMacSchedulerLcQos() override;
 
     /**
      * \brief Method to decide how to distribute the assigned bytes to the different LCs
      *        for the DL direction. This algorithm is based on the resource type and the
      *        guaranteed bitrate information of an LC.
+     *        In particular, the operation is divided in 4 parts:
+     *        1. The first part creates two lists, one with the GBR/DC-GBR active LCs that
+     *        have their ERAB guaranteed bit rate requirements set and one with all the
+     *        active LCs.
+     *        2. In case there more than 1 GBR/DC-GBR active LCs that have their ERAB
+     *        guaranteed bit rate requirements set, and their total requirements exceed the
+     *        assigned bytes (tbs), then the algorithm assigns equally all the assigned
+     *        bytes in RR fashion to these GBR/DC-GBR LCs.
+     *        3. In case their total requirements are less than the assigned bytes, the
+     *        algorithm assigns to each LC the minimum among the ERAB guaranteed bit rate
+     *        and the RLC buffer size.
+     *        4. The rest of the bytes, if any, are assigned in the rest of the LCs in RR
+     *        fashion.
+     *
      * \param ueLCG LCG of an UE
      * \param tbs TBS to divide between the LCG/LC
      * \return A vector of Assignation
      */
     std::vector<Assignation> AssignBytesToDlLC(const std::unordered_map<uint8_t, LCGPtr>& ueLCG,
-                                             uint32_t tbs, Time slotPeriod) const override;
+                                               uint32_t tbs,
+                                               Time slotPeriod) const override;
 
     /**
      * \brief Method to decide how to distribute the assigned bytes to the different LCs
@@ -64,9 +78,8 @@ class NrMacSchedulerLcQos : public NrMacSchedulerLcAlgorithm
      * \return A vector of Assignation
      */
     std::vector<Assignation> AssignBytesToUlLC(const std::unordered_map<uint8_t, LCGPtr>& ueLCG,
-                                             uint32_t tbs) const override;
-
+                                               uint32_t tbs) const override;
 };
-}
+} // namespace ns3
 
 #endif /*NR_MAC_SCHEDULER_LC_QOS_H*/
