16c65e42ccb82cb38707293f7b009bcc9081e86a
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2023-12-27 10:26:01 +0100

    nr: add black and isort formatter settings for python and apply them

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,27 +1,34 @@
-#Copyright (c) 2022 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
-#%SPDX-License-Identifier: GPL-2.0-only
+# Copyright (c) 2022 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+# %SPDX-License-Identifier: GPL-2.0-only
 
-import sys, os
+import os
+import sys
 
-sys.path.insert(0, os.path.abspath('extensions'))
+sys.path.insert(0, os.path.abspath("extensions"))
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
-                    'sphinx.ext.coverage', 'sphinx.ext.imgmath', 'sphinx.ext.ifconfig',
-                                  'sphinx.ext.autodoc']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.imgmath",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.autodoc",
+]
 
-latex_engine = 'xelatex'
+latex_engine = "xelatex"
 todo_include_todos = True
-templates_path = ['_templates']
-source_suffix = '.rst'
-master_doc = 'nr-module'
+templates_path = ["_templates"]
+source_suffix = ".rst"
+master_doc = "nr-module"
 exclude_patterns = []
 add_function_parentheses = True
-#add_module_names = True
-#modindex_common_prefix = []
+# add_module_names = True
+# modindex_common_prefix = []
 
-project = u'NR Module'
-copyright = u'2023'
-author = u'OpenSim CTTC/CERCA'
+project = "NR Module"
+copyright = "2023"
+author = "OpenSim CTTC/CERCA"
 
-version = '2.5'
-release = '2.5'
+version = "2.5"
+release = "2.5"
